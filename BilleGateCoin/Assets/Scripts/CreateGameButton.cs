62785708968c0736c6ef4c68fb2eb0e9212beca2
Aurèle Oulès <aurele@oules.com>
2020-04-09 23:39:29 +0200

    added functions

---
+++ BilleGateCoin/Assets/Scripts/CreateGameButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGameButton : MonoBehaviour
+{   
+    
+    public void AddMarble() {
+        GameObject createGameState = GameObject.FindGameObjectWithTag("CreateGameState");
+        
+        createGameState.GetComponent<CreateGameState>().marbles.Add("HELLO");
+    }
+}
---
+++ BilleGateCoin/Assets/Scripts/CreateGameState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGameState : MonoBehaviour
+{
+    public List<string> marbles;
+    
+
+
+}
