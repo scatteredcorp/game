06b50e9f2340c68a7e38685e05f6fb960a6b1a70
raphael.brenn <raphael.brenn@epita.fr>
2020-04-19 21:48:10 +0200

    fully working, deterministic + added ArrowLength variable

--- BilleGateCoin/Assets/Scripts/Player/ShotControl.cs
+++ BilleGateCoin/Assets/Scripts/Player/ShotControl.cs
@@ -14,6 +14,7 @@ public class ShotControl : MonoBehaviour
     public GameObject powerPreviewArrow;
     public Camera camera;
     public TextMeshPro powerPreviewNumber;
+    public float ArrowLength;
     public float Power;
     private bool wasPressed;
     private float pow;
@@ -30,97 +31,62 @@ public class ShotControl : MonoBehaviour
 
     private void FixedUpdate()
     {
-
-        if(player.GetComponent<Rigidbody>().velocity == new Vector3(0, 0, 0)) {
-            shootIsRunning = false;
-        }
-        
         if (Input.GetMouseButton(0))
         {
             wasPressed = true;
             powerPreview.SetActive(true);
             powerPreviewArrow.SetActive(true);
             powerPreviewNumber.enabled = true;
+            pow += -Input.GetAxis("Mouse Y") * 9;
             if ((pow < 0))
             {
-                powerPreview.transform.localPosition = Vector3.zero;
-                powerPreviewArrow.transform.localPosition = Vector3.zero;
-                powerPreview.transform.localScale = new Vector3(0.05f, 1f, 0.01f);
                 pow = 0;
             }
             else
             {
-                if ((pow * 6 > 100))
+                if ((pow > 100))
                 {
-                    powerPreview.transform.Translate(0, 0, -1 / 200f);
-                    powerPreviewArrow.transform.Translate(0, 0, 1 / 100f);
-                    powerPreview.transform.localScale += new Vector3(0, 0, -1 / 1000f);
-                    pow -= 0.1f;
-                }
-                else
-                {
-                    powerPreview.transform.Translate(0, 0, -Input.GetAxis("Mouse Y") / 20f);
-                    powerPreviewArrow.transform.Translate(0, 0, Input.GetAxis("Mouse Y") / 10f);
-                    powerPreview.transform.localScale += new Vector3(0, 0, -Input.GetAxis("Mouse Y") / 100f);
-                    pow += -Input.GetAxis("Mouse Y");
-                    if (pow >= 0)
-                    {
-                        if (pow * 6 <= 100)
-                        {
-                            powerPreviewNumber.text = Convert.ToString(Convert.ToInt32(pow * 6)) + "%";
-                        }
-                        else
-                        {
-                            powerPreviewNumber.text = "100%";
-                        }
-                    }
-                    else
-                    {
-                        powerPreviewNumber.text = "0%";
-                    }
+                    pow = 100;
                 }
             }
         }
         else
         {
-            if (wasPressed && Physics.Raycast(transform.position, -Vector3.up, 0.6f))
+            powerPreview.SetActive(false);
+            powerPreviewArrow.SetActive(false);
+            powerPreviewNumber.enabled = false;
+            if (wasPressed)
             {
                 wasPressed = false;
-                Shoot();
-                StartCoroutine(LaunchEffect());
+                if (Physics.Raycast(transform.position, -Vector3.up, 0.6f))
+                {
+                    Shoot();
+                    StartCoroutine(LaunchEffect());
+                }
             }
             else
             {
                 if (!shootIsRunning)
                 {
-                    powerPreview.SetActive(false);
-                    powerPreviewArrow.SetActive(false);
-                    powerPreviewNumber.enabled = false;
-                    powerPreview.transform.localPosition = Vector3.zero;
-                    powerPreviewArrow.transform.localPosition = Vector3.zero;
-                    powerPreview.transform.localScale = new Vector3(0.05f, 1f, 0.01f);
                     pow = 0;
                 }
             }
         }
+
+        powerPreview.transform.localPosition = new Vector3(0f, pow / 400f, pow / 200f) * ArrowLength;
+        powerPreviewArrow.transform.localPosition = new Vector3(0, pow / 200f, pow / 100f) * ArrowLength;
+        powerPreview.transform.localScale = new Vector3(0, 0, pow / 900f) * ArrowLength + new Vector3(0.05f, 1f, 0.01f);
+        powerPreviewNumber.text = Convert.ToString(Convert.ToInt32(pow)) + "%";
     }
-    
+
     void Shoot()
     {
         shootIsRunning = true;
         Vector3 force = (transform.forward * 4 + transform.up * 2) * pow * Power;
-        if(force.x > 300) force.x = 300;
-        if(force.x < -300) force.x = -300;
-        
-        if(force.y > 150) force.y = 150;
-        if(force.y < -150) force.y = -150;
-
-        if(force.z > 300) force.z = 300;
-        if(force.z < -300) force.z = -300;
 
         Debug.Log(force);
         player.GetComponent<Rigidbody>().AddForce(force);
-        player.GetComponent<Rigidbody>().AddTorque(force.z * 0.2f, 0, -force.x * 0.2f);
+        player.GetComponent<Rigidbody>().AddTorque(transform.right * 10);
         shootIsRunning = false;
     }
 
