d6f6d2355b25eba9aa4c8ce41b3ea4fab037d6f3
Aurèle Oulès <aurele@oules.com>
2020-04-20 12:54:00 +0200

    added obstacles

--- BilleGateCoin/Assets/Scripts/Game/GameManager.cs
+++ BilleGateCoin/Assets/Scripts/Game/GameManager.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+
 public class GameManager : MonoBehaviour {  
 
-    public uint Obstacles = 20;
-    public GameObject Obstacle;
+    public uint ObstaclesNumber;
+    public List<GameObject> Obstacles;
     private string hash = "8cf28eb9ac221d8cd15298b9ae63eca910b536a5234c133c7e364b29a4e39d21";
 
     private GameObject ObstaclesContainer;
@@ -11,11 +13,14 @@ public class GameManager : MonoBehaviour {
 
         Random.InitState(int.Parse(hash.Substring(0, hash.Length/8), System.Globalization.NumberStyles.HexNumber));
         
-        for(int i = 0; i < Obstacles; i++) {
-            float x = Random.Range(-54, -26);
-            float z = Random.Range(-36, -8);
+        for(int i = 0; i < ObstaclesNumber; i++) {
+            Debug.Log(Obstacles);
+            for(int j = 0; j < Obstacles.Count; j++) {
+                float x = Random.Range(-175, -40);
+                float z = Random.Range(-123, 19);
 
-            Instantiate(Obstacle, new Vector3(x, 5, z), Quaternion.identity);
+                Instantiate(Obstacles[j], new Vector3(x, 15, z), new Quaternion(90, 0, 0, -90));
+            }
         }
     }
 }
